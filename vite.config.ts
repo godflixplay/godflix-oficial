133d5353fa201bbd778d8f36cdad3a16733eb0a6
godflixplay <godflixplay@gmail.com>
2026-04-17 00:29:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
+import tsCConfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
+import { vercel } from "@tanstack/react-start/adapters/vercel";
 
 export default defineConfig({
   plugins: [
-    tsConfigPaths(),
+    tsCConfigPaths(),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({ adapter: vercel() }),
   ],
   resolve: {
     alias: {
