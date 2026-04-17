c7d1f1934bcac0d1b7098f204b14e11572b7cb6f
godflixplay <godflixplay@gmail.com>
2026-04-17 00:42:55 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
+import { createServer } from 'node:http';
 import handler from '../dist/server/server.js';
 
-export default async function(req) {
-  return handler.fetch(req);
+export default async function(req, res) {
+  return handler.fetch(req, {
+    env: process.env
+  });
 }
